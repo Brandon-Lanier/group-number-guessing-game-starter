61a35ae440dba7f86ea0c2b893c87961ea8eebac
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-03 14:40:18 -0600

    This will render an image and a new game button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,6 +9,7 @@ console.log('random num =', randomNumber);
 function handleReady() {
   console.log("jquery is loaded!")
   $('#submit-button').on('click', getInputs)
+  $('#steve').on('click', '#party-time', log)
 }
 
 function getInputs(){ 
@@ -54,7 +55,7 @@ function checkGuesses(arr) {
   console.log(arr);
   for (let guess of arr){
     if (guess.number === randomNumber){
-      // console.log('Winner!')
+      // winner();
       guess.result = 'Winnerrrrrr!';
       console.log(guess.result);
       
@@ -80,9 +81,28 @@ function checkGuesses(arr) {
       for (let j = 0; j < allGuesses[i].length; j++){
         console.log(allGuesses[i][j].name);
         $(`#${allGuesses[i][j].name}`).append(`<p>Number: ${allGuesses[i][j].number}, Result: ${allGuesses[i][j].result}</p>`);
-
+        winner(allGuesses[i][j]);
       }
       $('#counter-number').empty();
   } $('#counter-number').append(counter);
 }
 
+function winner(guess) {
+  // confirm('You Have A Winner!  Play Again??')
+  // console.log('this si the guess', guess);
+  if(guess.result === 'Winnerrrrrr!'){
+    $('#steve').append('<img src="https://cdn.mos.cms.futurecdn.net/FJgmXmkJyXrbEwgCwfjcaa.jpg">')
+    $('#steve').append('<button id="party-time">New Game?</button>')
+    
+  }
+  //show who won
+  //play again?
+  //reload the page
+
+
+}
+
+function log() {
+  console.log('haaaaaaai');
+  
+}
