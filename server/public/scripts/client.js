8c27f0e20d96b94180283585954c8267e5567509
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-03 11:15:39 -0600

    random number gen working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,23 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  rando();
+  $('#submit-button').on('click', getInputs)
 }
 
+function rando(){
+let randomNumber = Math.floor(Math.random() * ( 26 - 1 ) + 1); 
+  console.log(randomNumber);
+  return randomNumber;
+}
+
+function getInputs(){ 
+    let ourGuesses = [];
+    let andy = $("#andy-in").val();
+    let brandon = $("#brandon-in").val();
+    let colin = $("#colin-in").val();
+    let koffi = $("#koffi-in").val();
+
+console.log(andy, brandon, colin, koffi);
+
+}
