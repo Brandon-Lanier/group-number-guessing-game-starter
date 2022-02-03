f71a7c4e95e9f0343dd06692a1864b2d9f2e7e7e
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-03 16:56:06 -0600

    Commiting before end of day

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,17 +3,25 @@ $(document).ready(handleReady);
 
 let counter = 0;
 
-console.log('random num =', randomNumber);
-
-
 function handleReady() {
-  console.log("jquery is loaded!")
-  $('#submit-button').on('click', getInputs)
-  $('#steve').on('click', '#party-time', log)
+  console.log("jquery is loaded!");
+  $('#submit-button').on('click', getInputs);
+  // $('#steve').on('click', '#party-time', log);
+  getGuesses();
 }
 
+function getGuesses(){
+  $.ajax({
+    method: 'GET',
+    url: '/guesses'
+  }).then(function(response){
+    console.log('we did it', response);
+    // renderGuesses(response);
+  })
+}
+
+
 function getInputs(){ 
-  
     let andyIn = $("#andy-in").val();
     let brandonIn = $("#brandon-in").val();
     let colinIn = $("#colin-in").val();
@@ -23,30 +31,33 @@ function getInputs(){
       method: 'POST',
       url: '/guesses',
       data: {
-        guessesToAdd: {
+        guessesToAdd: [{
           name: 'andy',
           number: Number(andyIn),
           result: ''
         },
-        guessesToAdd: {
+        {
           name: 'brandon',
           number: Number(brandonIn),
           result: ''
         },
-        guessesToAdd: {
+        {
           name: 'colin',
           number: Number(colinIn),
           result: ''
         },
-        guessesToAdd: {
+        {
           name: 'koffi',
           number: Number(koffiIn),
           result: ''
         }
-      }
-    }).then(function(response){
-
+      ]}
+      }).then(function(response){
+      console.log('response is', response);
+      
     })
+  }
+}
     // let obj = [{
     //   name: 'andy',
     //   number: Number(andyIn),
@@ -67,70 +78,65 @@ function getInputs(){
     //   number: Number(koffiIn),
     //   result: '',
     // }];
-    counter ++
-    allGuesses.push(obj);
-    checkGuesses(obj);
+  //   counter ++
+  //   allGuesses.push(obj);
+  //   checkGuesses(obj);
    
    
     
-  } else {
-    alert('All inputs required');
-  }
-  $('.inputs').val('');
-}
+  // } else {
+  //   alert('All inputs required');
+  // }
+  // $('.inputs').val('');
+// }
 
-function checkGuesses(arr) {
-  console.log(arr);
-  for (let guess of arr){
-    if (guess.number === randomNumber){
-      // winner();
-      guess.result = 'Winnerrrrrr!';
-      console.log(guess.result);
+// function checkGuesses(arr) {
+//   console.log(arr);
+//   for (let guess of arr){
+//     if (guess.number === randomNumber){
+//       // winner();
+//       guess.result = 'Winnerrrrrr!';
+//       console.log(guess.result);
       
-    } else if (guess.number > randomNumber) {
-      // console.log('toomuuuuch') ;
-      guess.result = 'Too much!';
-      console.log(guess.result);
-    } else {
-      // console.log('Not enough');
-      guess.result = 'Too Low!';
-      console.log(guess.result);
-    }
-    }
-    renderGuesses()
-  }
+//     } else if (guess.number > randomNumber) {
+//       // console.log('toomuuuuch') ;
+//       guess.result = 'Too much!';
+//       console.log(guess.result);
+//     } else {
+//       // console.log('Not enough');
+//       guess.result = 'Too Low!';
+//       console.log(guess.result);
+//     }
+//     }
+//     renderGuesses()
+//   }
 
-  function renderGuesses(){
-    console.log('HELOOOOOOOO');
-    console.log(allGuesses);
-    $('.outDivs').empty();
-    for (let i = 0; i < allGuesses.length; i++){
-      console.log(allGuesses[i].name);
-      for (let j = 0; j < allGuesses[i].length; j++){
-        console.log(allGuesses[i][j].name);
-        $(`#${allGuesses[i][j].name}`).append(`<p>Number: ${allGuesses[i][j].number}, Result: ${allGuesses[i][j].result}</p>`);
-        winner(allGuesses[i][j]);
-      }
-      $('#counter-number').empty();
-  } $('#counter-number').append(counter);
-}
+//   function renderGuesses(){
+//     console.log('HELOOOOOOOO');
+//     console.log(allGuesses);
+//     $('.outDivs').empty();
+//     for (let i = 0; i < allGuesses.length; i++){
+//       console.log(allGuesses[i].name);
+//       for (let j = 0; j < allGuesses[i].length; j++){
+//         console.log(allGuesses[i][j].name);
+//         $(`#${allGuesses[i][j].name}`).append(`<p>Number: ${allGuesses[i][j].number}, Result: ${allGuesses[i][j].result}</p>`);
+//         winner(allGuesses[i][j]);
+//       }
+//       $('#counter-number').empty();
+//   } $('#counter-number').append(counter);
+// }
 
-function winner(guess) {
-  // confirm('You Have A Winner!  Play Again??')
-  // console.log('this si the guess', guess);
-  if(guess.result === 'Winnerrrrrr!'){
-    $('#steve').append('<img src="https://cdn.mos.cms.futurecdn.net/FJgmXmkJyXrbEwgCwfjcaa.jpg">')
-    $('#steve').append('<button id="party-time">New Game?</button>')
+// function winner(guess) {
+//   // confirm('You Have A Winner!  Play Again??')
+//   // console.log('this si the guess', guess);
+//   if(guess.result === 'Winnerrrrrr!'){
+//     $('#steve').append('<img src="https://cdn.mos.cms.futurecdn.net/FJgmXmkJyXrbEwgCwfjcaa.jpg">')
+//     $('#steve').append('<button id="party-time">New Game?</button>')
     
-  }
+//   }
   //show who won
   //play again?
   //reload the page
 
 
-}
-
-function log() {
-  console.log('haaaaaaai');
-  
-}
+// }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 const randomNumber = Math.floor(Math.random() * ( 26 - 1 ) + 1); 
-
+let allGuesses = require('./modules/guesses')
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.get('/guesses', function(req, res){
+  res.send(allGuesses)
+  
+})
+
 app.post('/guesses', function (req, res){
   let round = req.body.guessesToAdd;
   allGuesses.push(round);
